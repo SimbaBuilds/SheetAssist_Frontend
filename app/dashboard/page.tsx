e4a9a694163f70924741775b1d6428587ad9a2f8
Cameron Hightower <144738448+SimbaBuilds@users.noreply.github.com>
2024-11-11 15:50:31 -0700

    fixed console errors

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,12 +3,8 @@
 import { createClient } from '@/utils/supabase/client'
 import { DashboardPage } from '@/components/pages/DashboardPage'
 
-export default async function Dashboard() {
-  const supabase = createClient()
-  
-  // Fetch any initial data needed for the dashboard
-  const { data: initialData, error } = await supabase.from('your_table').select()
-  
+export default function Dashboard() {
+
   // Pass the initial data to the client component
-  return <DashboardPage initialData={initialData} />
+  return <DashboardPage/>
 } 
