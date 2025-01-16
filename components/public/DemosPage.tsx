06828173ad0a0732852b3fdef022dfc772bc4a46
Cameron Hightower <144738448+SimbaBuilds@users.noreply.github.com>
2025-01-16 00:32:04 -0600

    force-dynamic

--- components/public/DemosPage.tsx
+++ components/public/DemosPage.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+export const dynamic = 'force-dynamic'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import Link from "next/link"
 import Image from "next/image"
