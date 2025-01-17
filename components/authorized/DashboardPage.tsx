34626a21cc524ecf98b236bdbce96152141cdfc6
Cameron Hightower <144738448+SimbaBuilds@users.noreply.github.com>
2025-01-17 12:39:45 -0600

    gen dev (x button)

--- components/authorized/DashboardPage.tsx
+++ components/authorized/DashboardPage.tsx
@@ -347,6 +347,7 @@ export default function DashboardPage() {
                             size="sm"
                             onClick={() => removeSelectedUrlPair(index)}
                             className="ml-2"
+                            type="button"
                           >
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                               <line x1="18" y1="6" x2="6" y2="18"></line>
