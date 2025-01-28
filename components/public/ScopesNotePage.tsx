0ddfdd98e9b468d31076e682b65fc5a49858d84f
Cameron Hightower <144738448+SimbaBuilds@users.noreply.github.com>
2025-01-28 11:20:11 -0600

    Update ScopesNotePage.tsx

--- components/public/ScopesNotePage.tsx
+++ components/public/ScopesNotePage.tsx
@@ -12,7 +12,7 @@ export default function ScopesNotePage() {
            This application can integrate with your Google and Microsoft services to further streamline your workflows. Due to limitations in these services&apos; permission systems, we must request broader access than ideal:
         </p>
         <ul className="list-disc pl-10 text-sm text-foreground space-y-2">
-          <li>Google Drive: Google&apos;s drive.file scope (designed for file-specific permissions) allows reading to user specified files but not writing to them. To append data to your spreadsheets, we must request write access to all your spreadsheets.  Unfortunately, this Google scope also includes “delete” access.  Additionally, we ask for drive read-all access for our document and sheet title fetching feature in the dashboard interface.</li>
+          <li>Google Drive: Google&apos;s drive.file scope (designed for file-specific permissions) allows the reading of user specified files but not the writing to them. To append data to your spreadsheets, we must request write access to all your spreadsheets.  Unfortunately, this Google scope also includes “delete” access.  Additionally, we ask for drive read-all access for our document and sheet title fetching feature in the dashboard interface.</li>
           <li>Microsoft OneDrive: Microsoft appears to provide more granular permissions, but, unfortunately, their user consent screen still states, “read, write, and delete all files the user has access to,” even when the user specifies only file-specific permissions.</li>
         </ul>
       </div>
