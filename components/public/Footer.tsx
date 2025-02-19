0c8ff0ce0edec912ba16cfd99a6487eb65afc744
Cameron Hightower <144738448+SimbaBuilds@users.noreply.github.com>
2025-02-18 22:35:33 -0600

    Update Footer.tsx

--- components/public/Footer.tsx
+++ components/public/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="bg-background mt-auto">
       <div className="container mx-auto px-6 py-4">
         <div className="flex justify-between items-center">
-          <p className="text-foreground">&copy; 2024 Reggie. All rights reserved.</p>
+          <p className="text-foreground">&copy; 2024 SheetAssist. All rights reserved.</p>
           <div className="flex space-x-4">
             <Link href="/contact-us" className="text-foreground hover:text-primary">Contact Us</Link>
             <Link href="/privacy-policy" className="text-foreground hover:text-primary">Privacy Policy</Link>
