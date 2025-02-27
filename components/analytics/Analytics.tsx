109a3cd43da979feebd9956758e96c088024b309
Cameron Hightower <144738448+SimbaBuilds@users.noreply.github.com>
2025-02-26 20:05:34 -0600

    Update Analytics.tsx

--- components/analytics/Analytics.tsx
+++ components/analytics/Analytics.tsx
@@ -2,10 +2,9 @@
 
 import Script from "next/script"
 
-export default function Analytics() {
+export default function GoogleAnalytics() {
   return (
     <>
-      {/* Google Analytics */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=AW-16893823010"
         strategy="afterInteractive"
@@ -17,16 +16,6 @@ export default function Analytics() {
           gtag('js', new Date());
 
           gtag('config', 'AW-16893823010');
-        `}
-      </Script>
-
-      {/* Twitter/X Analytics */}
-      <Script id="twitter-analytics" strategy="afterInteractive">
-        {`
-          !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-          },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
-          a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-          twq('config','p6pi8');
         `}
       </Script>
     </>
